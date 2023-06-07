744b8857edc34f4e9c58312b758030f3ce320702
abaykurbanov <135601742+abaykurbanov@users.noreply.github.com>
2023-06-07 14:50:47 +0600

    5 june

---
+++ 05.06.23_1ex/my_math.js
@@ -0,0 +1,21 @@
+const add=function(a,b,c){
+    return a+b+c
+    
+}
+
+const cat=function (){
+    console.log( `
+ /\\_/\\  
+( o.o ) 
+ > ^ < 
+`)
+}
+
+
+module.exports={
+    add:add,
+    // minus:function(a,b,c,d){
+    //     return a-b
+    // },
+    cat
+}
